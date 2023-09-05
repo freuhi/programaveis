c4ec94397541368e05d2a60d910c54fd4e2dcac1
freuhi <139878930+freuhi@users.noreply.github.com>
2023-09-05 08:51:35 -0300

    Update Mecabraco.ino

--- Projetos com microcrontroladores/Mecabraco/Mecabraco.ino
+++ Projetos com microcrontroladores/Mecabraco/Mecabraco.ino
@@ -1,23 +1,23 @@
  
 #include <Servo.h> 
 
-Servo servo1; //cria um novo objeto servo
-Servo servo2;
-Servo servo3;
+Servo servo1; //cria um novo objeto servo1
+Servo servo2; //cria um novo objeto servo2
+Servo servo3; //cria um novo objeto servo3
 
-int potenciometroPino1 = 0;
-int potenciometroPino2 = 1;
-int potenciometroPino3 = 2;//conecta potenciometro na porta analogica 0
+int potenciometroPino1 = 0;//conecta potenciometro na porta analogica 0
+int potenciometroPino2 = 1;//conecta potenciometro na porta analogica 1
+int potenciometroPino3 = 2;//conecta potenciometro na porta analogica 2
 
-int valorDoPotenciometro1 = 0;
-int valorDoPotenciometro2 = 0;
-int valorDoPotenciometro3 = 0;//valor lido no pino 0
+int valorDoPotenciometro1 = 0;//valor lido no pino 0
+int valorDoPotenciometro2 = 0;//valor lido no pino 1
+int valorDoPotenciometro3 = 0;//valor lido no pino 2
 
 void setup (){
 	
-  servo1.attach(2);
-  servo2.attach(3);
-  servo3.attach(4);//conecta o objeto servo1 ao pino 4
+  servo1.attach(2);//conecta o objeto servo1 ao pino 2
+  servo2.attach(3);//conecta o objeto servo2 ao pino 3
+  servo3.attach(4);//conecta o objeto servo3 ao pino 4
   
   
 }//fim setup
